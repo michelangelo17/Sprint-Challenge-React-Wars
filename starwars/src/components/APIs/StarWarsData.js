31c7ade38fed6d55782a79a3e71171785d95efdd
michelangelo17 <michelangelo.markus@gmail.com>
2019-11-08 08:46:28 -0800

    api is working

---
+++ starwars/src/components/APIs/StarWarsData.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
+
+const StarWarsData = () => {
+  const [characters, setCharacters] = useState(null);
+  useEffect(() => {
+    Axios.get(
+      'https://swapi.co/api/people/'
+    )
+    .then(res => {
+      const nextPage = res.data.next;
+      const prevPage = res.data.previous;
+      const tenCharacters = res.data.results;
+      console.log(nextPage, prevPage);
+      return setCharacters(tenCharacters);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }, [])
+  return characters;
+}
+
+export default StarWarsData;
